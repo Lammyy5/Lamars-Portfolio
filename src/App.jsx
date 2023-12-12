870ce4afee45c95cae52f3097958fb6f9c716753
Lammyy5 <reliford2010@gmail.com>
2023-12-11 17:30:20 -0800

    added component files for header,footer, and navbar also linked heade r and footer to app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+
+
+  return (
+    
+      <div>
+        <Header />
+       My name is Lamar Reliford I am a 24 year old coding bootcamp student at UCR. I started my education journey at Riverside Community College as a kinesiology major but quickly realized it wasnt the major for me. As a result of my realization I switched my major to business which instrested me since a lot of people i know own businesses. However I ended up taking a break from school to focus on work and educating myself. In my pursuit of new opportunity I
+    stumbled across coding and have been instrested in growing in this field.
+    I already learned some html from some free sites that I found online but never got around to finish learning this
+    skill.
+        <Footer />
+    </div>
+  )
+}
+
+export default App
